01e9baec42c9778f5eab8d6a4b6850c99a0d89ae
Sanjay Garg <sgarg5858@gmail.com>
2022-08-06 14:07:24 +0530

    retry strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { ForkJoinComponent } from './fork-join/fork-join.component';
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
+    SearchbarComponent,
+    ForkJoinComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, of } from 'rxjs';
+import { catchError, concat, concatMap, delay, of, retryWhen, scan, take, throwError } from 'rxjs';
 
 export interface Post{
   userId:number;
@@ -18,8 +18,28 @@ export class PostService {
 
   getPosts()
   {
-    return this.httpClient.get<Post[]>('https://jsonplaceholder.typicode.com/posts').pipe(
-      catchError((err)=>of([]))
+    return this.httpClient.get<Post[]>('https://jsonplaceholder.typicode.com/poasts').pipe(
+      retryWhen((errors)=>
+      // concat(
+      //   errors.pipe(
+      //     delay(2000),
+      //     take(2)
+      //   ),
+      //   throwError("Max tries exceeded!")
+      // )
+      errors.pipe(
+        concatMap((error,index)=>{
+          console.log(index);
+          if(index>2)
+          {
+            return throwError(()=>error);
+          }
+          else return of(error).pipe(delay(2000))
+        })
+      )
+      )
+      ,
+      catchError((err)=>{console.log(err);return of([])})
     )
   }
 }
